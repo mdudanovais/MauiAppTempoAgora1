3a1b5c282b77a8f365310cdb955da4d4e53567e2
mdudanovais <diana-novais@hotmail.com>
2025-04-06 23:40:06 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppTempoAgora1/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using MauiAppTempoAgora1.Models;
+using MauiAppTempoAgora1.Services;
+
+namespace MauiAppTempoAgora1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
+
+                    if (t != null)
+                    {
+                        string dados_previsao = "";
+
+                        dados_previsao = $"Latitude: {t.lat} \n" +
+                                        $"Longitude: {t.lon} \n" +
+                                        $"Temperatura Minima: {t.temp_min} \n" +
+                                        $"Temperatura Maxima: {t.temp_max} \n" +
+                                        $"Descrição: {t.description} \n" +
+                                        $"Main: {t.main} \n" +
+                                        $"Velocidade do Vento: {t.speed} \n" +
+                                        $"Visibilidade: {t.visibility} \n" +
+                                        $"Nascer do Sol: {t.sunrise} \n" +
+                                        $"Pôr do Sol: {t.sunset} \n";
+
+                        lbl_res.Text = dados_previsao;
+
+
+                    }
+                    else
+                    {
+                        lbl_res.Text = "Sem dados de Previsão";
+                    }
+
+                }
+                else
+                {
+                    lbl_res.Text = "Preencha a cidade.";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+    }
+}
+    }
+
+}
---
+++ MauiAppTempoAgora1/Services/DataService.cs
@@ -0,0 +1,72 @@
+﻿using MauiAppTempoAgora1.Models;
+using Newtonsoft.Json.Linq;
+using System.Net;
+
+namespace MauiAppTempoAgora1.Services
+{
+    public class DataService
+    {
+        public static async Task<Tempo?> GetPrevisao(string cidade)
+        {
+            Tempo? t = null;
+
+            string chave = "d9f149a1a4e169be767f3fdaeb133070";
+            string url = $"https://api.openweathermap.org/data/2.5/weather?" +
+                $"q={cidade}&units=metric&appid={chave}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+
+                    HttpResponseMessage resp = await client.GetAsync(url);
+
+                    if (resp.StatusCode == HttpStatusCode.NotFound)
+                    {
+                        throw new Exception("Cidade não encontrada, digite novamente.");
+                        // Mensagem específica para quando a cidade não for encontrada.
+
+                    }
+
+                    else if (resp.IsSuccessStatusCode)
+                    {
+                        string json = await resp.Content.ReadAsStringAsync();
+
+                        var rascunho = JObject.Parse(json);
+
+                        DateTime time = new();
+                        DateTime sunrise = time.AddSeconds((double)rascunho["sys"]["sunrise"]).ToLocalTime();
+                        DateTime sunset = time.AddSeconds((double)rascunho["sys"]["sunset"]).ToLocalTime();
+
+
+                        t = new()
+                        {
+                            lat = (double)rascunho["coord"]["lat"],
+                            lon = (double)rascunho["coord"]["lon"],
+                            description = (string)rascunho["weather"][0]["description"],
+                            main = (string)rascunho["weather"][0]["main"],
+                            temp_min = (double)rascunho["main"]["temp_min"],
+                            temp_max = (double)rascunho["main"]["temp_max"],
+                            speed = (double)rascunho["wind"]["speed"],
+                            visibility = (int)rascunho["visibility"],
+                            sunrise = sunrise.ToString(),
+                            sunset = sunset.ToString(),
+                        }; // Fecha obj do Tempo.
+                    } // Fecha if se o status do servidor foi de sucesso.
+                }
+                catch (HttpRequestException ex)
+                {
+                    throw new Exception("Erro ao acessar o servidor, tente novamente mais tarde.");
+                    // Mensagem específica para quando houver erro de conexão com o servidor.
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Erro inesperado: " + ex.Message);
+                    // Mensagem genérica para outros erros.
+                }
+
+            } // Fecha o using.
+            return t;
+        }
+    }
+}
